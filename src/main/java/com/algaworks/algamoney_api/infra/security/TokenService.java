eedad04e96f9fc0e6676f05a1dd126946676c327
Daniel Lincoln <lincolndeveloper@outlook.com>
2023-12-10 13:20:29 -0300

    Criando serviço para validar o Token JWT

--- src/main/java/com/algaworks/algamoney_api/infra/security/TokenService.java
+++ src/main/java/com/algaworks/algamoney_api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.algaworks.algamoney_api.model.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,9 +22,9 @@ public class TokenService {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             String token = JWT.create()
-                    .withIssuer("auth-api")
-                    .withSubject(usuario.getUsername())
-                    .withExpiresAt(generateExpirationDate())
+                    .withIssuer("auth-api")                     // passamos quem é o emissor
+                    .withSubject(usuario.getUsername())         // adicionamos o subject no token
+                    .withExpiresAt(generateExpirationDate())    // gera um tempo de expiração
                     .sign(algorithm);
             return token;
         }catch (JWTCreationException ex){
@@ -31,6 +32,20 @@ public class TokenService {
         }
     }
 
+    /**Método que valida o token para verificar se está válido*/
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret); // cria o algorithm
+            return JWT.require(algorithm)                   // informamos o algorithn criado
+                    .withIssuer("auth-api")                 // passamos quem é o emissor
+                    .build()                                // montamos o dado dentro de algorithm
+                    .verify(token)                          // descriptografamos o token
+                    .getSubject();                          // pegamos o subject que tínhamos salvo
+
+        } catch (JWTVerificationException ex){              //  Capturamos a exeção caso, o token for inválido.
+            return "";
+        }
+    }
 
     private Instant generateExpirationDate(){
         return LocalDateTime.now()
