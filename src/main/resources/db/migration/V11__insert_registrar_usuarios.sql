7c0883216867ba9687b58716bbd8f25e61f41f92
Daniel Lincoln <lincolndeveloper@outlook.com>
2023-12-14 21:26:54 -0300

    6.11. Movendo o usuário para o banco de dados

---
+++ src/main/resources/db/migration/V11__insert_registrar_usuarios.sql
@@ -0,0 +1,17 @@
+INSERT INTO usuario (codigo, role, nome, login, password) values (1, 'ADMIN', 'Administrador', 'admin@algamoney.com', '$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
+INSERT INTO usuario (codigo, role, nome, login, password) values (2, 'USER', 'Maria Silva', 'maria@algamoney.com', '$2a$10$Zc3w6HyuPOPXamaMhh.PQOXvDnEsadztbfi6/RyZWJDzimE8WQjaq');
+
+-- admin
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (1, 1);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (1, 2);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (1, 3);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (1, 4);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (1, 5);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (1, 6);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (1, 7);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (1, 8);
+
+-- maria
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (2, 2);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (2, 5);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) values (2, 8);
